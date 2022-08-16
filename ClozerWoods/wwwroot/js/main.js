8cc1c77aab6ab068b656d17a4c098381e1929095
Michael Pidde <michaelpidde@fastmail.com>
2022-08-16 00:05:55 -0500

    Abstract common javascript function

---
+++ ClozerWoods/wwwroot/js/main.js
@@ -0,0 +1,12 @@
+﻿const attachItemSwitchEvent = (id) => {
+    const element = document.getElementById(id);
+    if (element != null) {
+        element.onchange = () => {
+            var url = location.protocol + '//' + location.host + location.pathname;
+            if (element.value != "") {
+                url += `?${id}=${element.value}`;
+            }
+            location.href = url;
+        }
+    }
+}
