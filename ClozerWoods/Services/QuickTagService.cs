e81ec0407830b96ce2206c792d137237f2eba0c1
Michael Pidde <michaelpidde@fastmail.com>
2022-08-28 23:30:31 -0500

    Add some styling for content and some transformation code for 'quick tags'

---
+++ ClozerWoods/Services/QuickTagService.cs
@@ -0,0 +1,17 @@
+﻿namespace ClozerWoods.Services;
+
+public class QuickTagService
+{
+    public static string? Transform(string? content) {
+        if(content == null) {
+            return content;
+        }
+        const string completeCheckbox = "<input type=\"checkbox\" checked disabled class=\"completion-checkbox\">";
+        const string completeCheckboxToken = "[X]";
+        const string incompleteCheckbox = "<input type=\"checkbox\" disabled class=\"completion-checkbox\">";
+        const string incompleteCheckboxToken = "[]";
+        return content
+            .Replace(completeCheckboxToken, completeCheckbox)
+            .Replace(incompleteCheckboxToken, incompleteCheckbox);
+    }
+}
