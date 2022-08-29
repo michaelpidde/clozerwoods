1401ea8aaec621825562a52b093d5dbede98e927
Michael Pidde <michaelpidde@fastmail.com>
2022-08-28 23:58:25 -0500

    Add command to pass to putty to restart application service on deploy

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,8 @@
 @echo off
 
+REM TODO: Maybe change this to a Powershell script so we can accept the
+REM       password as an argument where the typed characters are obscured.
+
 cd D:\Code\ClozerWoods
 
 dotnet publish -p:PublishProfile=FolderProfile -c:Release
@@ -9,6 +12,8 @@ pushd D:\Code\ClozerWoods\ClozerWoods\bin\Release\net6.0\linux-x64\publish
 call "C:\Program Files (x86)\WinSCP\WinSCP.com" /ini=nul /script=D:\Code\ClozerWoods\deploy.winscp.txt
 
 popd
+
+putty.exe root@45.76.24.90 -i "C:\Users\micha\.ssh\clozerwoods.ppk" -m "deploy.putty.txt"
 
 echo.
 echo Done.
